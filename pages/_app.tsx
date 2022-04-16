22cdda5935eb2594d296be750cf5bc5c4f8c24b7
Robert Adams <adamsiii.robert@gmail.com>
2022-04-16 11:34:11 -0700

    prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 
 const App = ({ Component, pageProps }: AppProps) => {
-
   // use for conditional rendering if 404
   // const is404 = Component.displayName === 'ErrorPage';
   return <Layout page={<Component {...pageProps} />} />;
