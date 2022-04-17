fa4bf2ab0f2ee440b10188e1995534820f250af0
Robert Adams <adamsiii.robert@gmail.com>
2022-04-17 00:55:31 -0700

    created basic project page

---
+++ pages/projects/[id].tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { getProjectIds, getProjectData } from '../../lib/projects';
+import type { GetStaticProps, GetStaticPaths, NextPage } from 'next/types';
+import type { Project } from '../../@types/projects';
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const paths = getProjectIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = ({ params }) => {
+  const project = getProjectData(params!.id as string);
+  return {
+    props: {
+      project,
+    },
+  };
+};
+
+const ProjectPage: NextPage<{ project: Project }> = ({ project }) => {
+  return (
+    <>
+      <section>
+        <h1>{project.id}</h1>
+        <p>{project.desc}</p>
+      </section>
+      <section>
+        <Link href='/'>
+          <a>Back to Projects</a>
+        </Link>
+      </section>
+    </>
+  );
+};
+
+export default ProjectPage;
