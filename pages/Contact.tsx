7094cac938b8d9912a5b882d4ba85973033b1b30
Robert Adams <adamsiii.robert@gmail.com>
2022-04-16 21:34:48 -0700

    created contact page

---
+++ pages/Contact.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next/types';
+
+const Contact: NextPage = () => {
+  return (
+    <section id='contact'>
+      <h1>CONTACT</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        suscipit deleniti eligendi rem? Cupiditate nam sit omnis tempore iste
+        dolor, id enim mollitia vel sapiente illum est fugiat explicabo
+        blanditiis.
+      </p>
+      <br />
+      <p>RESUME</p>
+      <p>EMAIL</p>
+      <p>LINKEDIN</p>
+      <p>GITHUB</p>
+      <br/>
+    </section>
+  );
+};
+
+export default Contact;
