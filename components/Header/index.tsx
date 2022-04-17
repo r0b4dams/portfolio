f4c4d8e524272cd17705a4af872b9a10b9463b65
Robert Adams <adamsiii.robert@gmail.com>
2022-04-16 21:11:55 -0700

    added basic styles and imported Nav component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Nav from '../Nav';
+
+const styles = {
+  height: 'h-[10vh]',
+  flex: 'flex items-center',
+  color: 'bg-slate-900',
+};
 
 const Header: React.FC = (): JSX.Element => {
-  return <header className='h-[10vh]'>Header</header>;
+  return (
+    <header className={`${styles.height} ${styles.flex} ${styles.color}`}>
+      <Nav />
+    </header>
+  );
 };
 
 export default Header;
