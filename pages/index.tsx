9a30bb5f9f3a1c14e4facd3acac185801676b462
Robert Adams <adamsiii.robert@gmail.com>
2022-04-16 21:36:54 -0700

    added images and placeholder text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next/types';
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +10,64 @@ const Home: NextPage = () => {
         <title>{`Robert Adams' Portfolio Home`}</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div>Main</div>
+      <section id='intro'>
+        <Image src='/images/avatar.jpg' alt='avatar' height={144} width={144} />
+        <h1>Salutation!</h1>
+        <p>Some additional intro text!</p>
+        <p>Welcome to my portfolio!</p>
+      </section>
+      <section id='projects'>
+        <Link href='/'>
+          <a>
+            <Image
+              src='/images/traductora-demo.gif'
+              alt='console log demo'
+              height={180}
+              width={320}
+            />
+          </a>
+        </Link>
+        <Link href='/'>
+          <a>
+            <Image
+              src='/images/console-log-demo.gif'
+              alt='console log demo'
+              height={180}
+              width={320}
+            />
+          </a>
+        </Link>
+        <Link href='/'>
+          <a>
+            <Image
+              src='/images/pantrimonium-demo.gif'
+              alt='console log demo'
+              height={180}
+              width={320}
+            />
+          </a>
+        </Link>
+        <Link href='/'>
+          <a>
+            <Image
+              src='/images/vedomy-demo.gif'
+              alt='console log demo'
+              height={180}
+              width={320}
+            />
+          </a>
+        </Link>
+        <Link href='/'>
+          <a>
+            <Image
+              src='/images/dad-jokes-demo.gif'
+              alt='console log demo'
+              height={180}
+              width={320}
+            />
+          </a>
+        </Link>
+      </section>
     </>
   );
 };
