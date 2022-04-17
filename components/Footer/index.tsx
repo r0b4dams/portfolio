0aac1a26ca47e5940038f741d7ff52d814b26429
Robert Adams <adamsiii.robert@gmail.com>
2022-04-16 21:29:56 -0700

    updated footer with basic styles

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+const styles = {
+  position: 'fixed bottom-0 left-0 right-0 z-[999]',
+  height: 'h-[5vh]',
+  flex: 'flex items-center',
+  bgColor: 'bg-slate-900',
+  txtColor: 'text-slate-50',
+};
+
 const Footer: React.FC = (): JSX.Element => {
-  return <footer>Footer</footer>;
+  return (
+    <footer
+      className={`${styles.position} ${styles.height} ${styles.flex} ${styles.bgColor} ${styles.txtColor}`}
+    >
+      Robert Adams
+    </footer>
+  );
 };
 
 export default Footer;
