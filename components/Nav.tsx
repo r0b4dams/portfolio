d2a31034a9463bc5282d43f0e3bf88f83ecfe7ab
Robert Adams <adamsiii.robert@gmail.com>
2022-04-21 20:56:00 -0700

    renamed component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
+import Logo from './Logo';
 import NavBurger from './NavBurger';
 import NavOverlay from './NavOverlay';
 
-const Navbar: React.FC = (): JSX.Element => {
+const Nav: React.FC = (): JSX.Element => {
   const [active, setActive] = useState(false);
-
   const toggleActive = () => setActive(() => !active);
-
   return (
-    <div id='nav-bar' className='flex items-center h-full w-full'>
+    <div
+      id='nav-bar'
+      className='flex items-center justify-between h-full w-full'
+    >
+      <Logo />
       <NavBurger active={active} toggleActive={toggleActive} />
       <NavOverlay active={active} toggleActive={toggleActive} />
     </div>
   );
 };
 
-export default Navbar;
+export default Nav;
