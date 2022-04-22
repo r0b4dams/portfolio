d4be3d2b89d907809682cc3c4bc017ec383ffbff
Robert Adams <adamsiii.robert@gmail.com>
2022-04-21 20:58:19 -0700

    added about text

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,27 +1,35 @@
 import type { NextPage } from 'next/types';
+import Image from 'next/image';
 
 const About: NextPage = () => {
   return (
     <section id='about'>
+      <Image src='/images/avatar.jpg' alt='avatar' height={144} width={144} />
       <h1>ABOUT</h1>
       <br />
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi modi
-        deleniti facere magni impedit aspernatur odio enim dolor commodi? Eum
-        cupiditate autem libero voluptatem, saepe vel nisi facilis culpa natus.
+        Coming out of college I accepted a position as an avionics repair
+        technician. It was a promising job and my adaptability and ability to
+        learn quickly paid off: in less than a year I had earned trainer status
+        in both the repair of in-flight hardware as well as the maintenance
+        repair software used for documentation.
       </p>
       <br />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        nisi laboriosam itaque beatae omnis. Error aut, sapiente iure
-        consequuntur animi exercitationem cum quisquam perferendis vel nobis
-        aspernatur nostrum voluptas esse!
+        Unfortunately, the uncertainty caused by the pandemic led to a lot of
+        restructuring and I felt like I’d hit a wall in my professional growth.
+        I realized I needed to make a change. I’d always enjoyed the programming
+        aspects of my physics degree, so when I saw a chance to apply for a full
+        stack web development certificate, I jumped at it.
       </p>
       <br />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nemo
-        harum unde non ea tenetur neque? Veritatis repellat suscipit beatae ad
-        ea sapiente, amet eos aspernatur dolorum magni mollitia nihil?
+        I’m glad I did, because in earning it, it really solidified my
+        appreciation for what I enjoy in a job: learning new things, problem
+        solving, and helping build something tangible. I’m eager to contribute
+        my skills to a collaborative environment, and look to find a position
+        where I can further develop current skills as well as acquire new ones.
+        I’m really excited and I look forward to learning more!
       </p>
       <br />
     </section>
