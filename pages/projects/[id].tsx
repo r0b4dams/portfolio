b8fa2e9bf8ac0fc9d9737b91272a9cf6eb535704
Robert Adams <adamsiii.robert@gmail.com>
2022-04-21 20:53:06 -0700

    added project info and icons

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,7 +1,10 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { getProjectIds, getProjectData } from '../../lib/projects';
 import type { GetStaticProps, GetStaticPaths, NextPage } from 'next/types';
 import type { Project } from '../../@types/projects';
+import FaGitHubIcon from '../../components/icons/FaGitHub';
+import FaDeployLinkIcon from '../../components/icons/FaDeploy';
 
 export const getStaticPaths: GetStaticPaths = () => {
   const paths = getProjectIds();
@@ -22,17 +25,39 @@ export const getStaticProps: GetStaticProps = ({ params }) => {
 
 const ProjectPage: NextPage<{ project: Project }> = ({ project }) => {
   return (
-    <>
-      <section>
-        <h1>{project.name}</h1>
-        <p>{project.desc}</p>
-      </section>
-      <section>
-        <Link href='/'>
-          <a>Back to Projects</a>
+    <section className='container m-auto flex flex-col items-center'>
+      <div>
+        <Image
+          src={`/images/${project.id}-demo.gif`}
+          alt=''
+          height={360}
+          width={640}
+        />
+      </div>
+      <h1>{project.name}</h1>
+      <p>{project.desc}</p>
+      <h2>Role</h2>
+      {project.role}
+      <h2>Tech</h2>
+      <p>{project.tech}</p>
+      <div>
+        <ul>
+          <li>
+            <a href={project.appURL} target='_blank' rel='noreferrer'>
+              Deployed App <FaDeployLinkIcon />
+            </a>
+          </li>
+          <li>
+            <a href={project.repoURL} target='_blank' rel='noreferrer'>
+              GitHub Repo <FaGitHubIcon />
+            </a>
+          </li>
+        </ul>
+        <Link href='/portfolio'>
+          <a>{'<< Back to Portfolio'}</a>
         </Link>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
