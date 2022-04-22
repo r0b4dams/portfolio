bfa2a10efcf005b0203d4b539c99a3dedb8ac240
Robert Adams <adamsiii.robert@gmail.com>
2022-04-21 20:54:11 -0700

    updated style with global class

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import Navbar from './Navbar';
-
-const styles = {
-  height: 'h-[10vh]',
-  flex: 'flex items-center',
-  color: 'bg-slate-900',
-};
+import Nav from './Nav';
 
 const Header: React.FC = (): JSX.Element => {
   return (
-    <header className={`${styles.height} ${styles.flex} ${styles.color}`}>
-      <Navbar />
+    <header className='portfolio-header'>
+      <Nav />
     </header>
   );
 };
