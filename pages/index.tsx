ae497b2cbcd352a7529aea2d272698d7e0a400ed
Robert Adams <adamsiii.robert@gmail.com>
2022-04-21 20:59:02 -0700

    moved props to portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,8 @@
 import Head from 'next/head';
-import Image from 'next/image';
+
 import Link from 'next/link';
-import { getAllProjectData } from '../lib/projects';
-import type { GetStaticProps, NextPage } from 'next/types';
-import type { ProjectData } from '../@types/projects';
 
-export const getStaticProps: GetStaticProps = async () => {
-  const projects = getAllProjectData();
-  return {
-    props: {
-      projects,
-    },
-  };
-};
-
-const Home: NextPage<{
-  projects: ProjectData;
-}> = ({ projects }) => {
+const Intro = () => {
   return (
     <>
       <Head>
@@ -25,25 +11,12 @@ const Home: NextPage<{
       </Head>
 
       <section id='intro'>
-        <Image src='/images/avatar.jpg' alt='avatar' height={144} width={144} />
-        <h1>Salutation!</h1>
-        <p>Some additional intro text!</p>
-        <p>Welcome to my portfolio!</p>
-      </section>
-
-      <section>
-        {projects.map((project) => {
-          return (
-            <li key={project.id}>
-              <Link href={`/projects/${project.id}`}>
-                <a>{project.name}</a>
-              </Link>
-            </li>
-          );
-        })}
+        <Link href='/portfolio'>
+          <a>GOTO portfolio</a>
+        </Link>
       </section>
     </>
   );
 };
 
-export default Home;
+export default Intro;
