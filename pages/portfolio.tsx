839ee008b9e22664b15c46e8d74c9a425114b1d5
Robert Adams <adamsiii.robert@gmail.com>
2022-04-22 19:12:39 -0700

    projects factored out to own component

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,9 +1,9 @@
 import { GetStaticProps, NextPage } from 'next';
-import Link from 'next/link';
-import Image from 'next/image';
-import { ProjectData } from '../@types/projects';
-import { getAllProjectData } from '../lib/projects';
 import Head from 'next/head';
+
+import ProjectContainer from '../components/ProjectContainer';
+import { getAllProjectData } from '../lib/projects';
+import { ProjectData } from '../@types/projects';
 
 export const getStaticProps: GetStaticProps = async () => {
   const projects = getAllProjectData();
@@ -14,34 +14,15 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-const styles = {
-  flex: 'flex flex-wrap',
-};
-
 const Portfolio: NextPage<{ projects: ProjectData }> = ({ projects }) => {
   return (
     <>
       <Head>
-        <title>{`Robert Adams' Portfolio`}</title>
+        <title>{'Robert Adams | Portfolio'}</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <section className={styles.flex}>
-        {projects.map((project) => (
-          <div key={project.id} className='w-1/2'>
-            <Link href={`/projects/${project.id}`}>
-              <a>
-                <Image
-                  src={`/images/${project.id}-demo.gif`}
-                  alt=''
-                  layout='responsive'
-                  height={360}
-                  width={640}
-                />
-              </a>
-            </Link>
-          </div>
-        ))}
-      </section>
+
+      <ProjectContainer projects={projects} />
     </>
   );
 };
