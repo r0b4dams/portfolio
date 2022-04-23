ac97f69a33ce7451f4963d375dcde84bbd8eca7a
Robert Adams <adamsiii.robert@gmail.com>
2022-04-22 19:13:18 -0700

    added basic style

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
 
-const Logo: React.FC = () => {
-  return (
-    <Link href='/'>
-      <a>RA</a>
-    </Link>
-  );
-};
+const Logo: React.FC = () => (
+  <Link href='/'>
+    <a className='text-slate-50 text-2xl px-[30px] relative z-[9999]'>RA</a>
+  </Link>
+);
 
 export default Logo;
