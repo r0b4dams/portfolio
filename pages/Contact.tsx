9b12cb7997b9a0c2ab1cf23ecca436942b5397d9
Robert Adams <adamsiii.robert@gmail.com>
2022-04-22 19:20:52 -0700

    added Head component and title meta

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,25 +1,28 @@
+import Head from 'next/head';
 import type { NextPage } from 'next/types';
-import FaGitHubIcon from '../components/icons/FaGitHub';
-import FaLinkedInIcon from '../components/icons/FaLinkedIn';
+import ContactCard from '../components/ContactCard';
 
 const Contact: NextPage = () => {
   return (
-    <section id='contact'>
-      <h1>CONTACT</h1>
-      <br />
-      <p>Contact me for professional opportunities or project collaboration!</p>
-      <p>I look forward to hearing from you!</p>
-      <br />
-      <p>RESUME</p>
-      <p>EMAIL</p>
-      <p>
-        LINKEDIN <FaLinkedInIcon />
-      </p>
-      <p>
-        GITHUB <FaGitHubIcon />
-      </p>
-      <br />
-    </section>
+    <>
+      <Head>
+        <title>{'Robert Adams | Contact'}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <section className='h-[40vh] p-[10px]'>
+        <h1 className='text-5xl'>{"Let's get in touch!"}</h1>
+        <p className='text-xl'>
+          Contact me for professional opportunities or project collaboration!
+        </p>
+      </section>
+      <section className='container flex flex-wrap items-center justify-around'>
+        <ContactCard src={'/icons/email.svg'} cardColor={'bg-slate-500'} />
+        <ContactCard src={'/icons/linkedin.svg'} cardColor={'bg-slate-600'} />
+        <ContactCard src={'/icons/github.svg'} cardColor={'bg-slate-700'} />
+        <ContactCard src={'/icons/resume.svg'} cardColor={'bg-slate-800'} />
+      </section>
+    </>
   );
 };
 
