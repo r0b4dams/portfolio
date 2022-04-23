4cf9785bc1bc53a6a95db8d94789d3e1112cdd26
Robert Adams <adamsiii.robert@gmail.com>
2022-04-22 19:19:24 -0700

    updated Icon with optional height width params

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,12 +1,22 @@
 import Image from 'next/image';
 
-const Icon: React.FC<{ src: string; className: string }> = ({
-  src,
-  className,
-}): JSX.Element => (
-  <div>
-    <Image src={src} alt='' className={className} height={25} width={25} />
-  </div>
-);
+const Icon: React.FC<{
+  src: string;
+  className: string;
+  height?: number;
+  width?: number;
+}> = ({ src, className, height = 30, width = 30 }): JSX.Element => {
+  return (
+    <div className='flex'>
+      <Image
+        src={src}
+        alt=''
+        className={className}
+        height={height}
+        width={width}
+      />
+    </div>
+  );
+};
 
 export default Icon;
