37618a5d2016d4f9851bce19cdb625a96e3bc4bd
Robert Adams <adamsiii.robert@gmail.com>
2022-04-25 16:08:32 -0700

    added types to add boolean to intro page component for conditional rendering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
 import '../styles/globals.css';
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Layout page={<Component {...pageProps} />} />;
+type NextPageWithLayout = NextPage & {
+  isIntro?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  return Component.isIntro ? (
+    <Component />
+  ) : (
+    <Layout page={<Component {...pageProps} />} />
+  );
 };
 
 export default App;
