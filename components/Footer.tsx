0508139d9279a30d253fa1ab6ce32e623ee288a6
Robert Adams <adamsiii.robert@gmail.com>
2022-05-09 16:25:18 -0700

    tweaked styles and removed links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import Icon from '../components/Icon';
+// import Icon from '../components/Icon';
 
 const year = new Date().getFullYear();
 
 const Footer: React.FC = (): JSX.Element => {
   return (
-    <footer className='portfolio-footer px-[30px] h-min-[30px]'>
+    <footer className='bg-white h-[5vh] border-t-4 border-blue-500'>
       <div>Robert Adams ©{year}</div>
-
-      <span className='flex'>
-        <a href='https://github.com/comatosino' className='px-[10px]'>
-          <Icon
-            src='/icons/github.svg'
-            className='bg-slate-50 rounded-md flex'
-          />
-        </a>
-        <a href='https://www.linkedin.com/in/robert-adams-comatosino'>
-          <Icon
-            src='/icons/linkedin.svg'
-            className='bg-slate-50 rounded-md flex'
-          />
-        </a>
-      </span>
     </footer>
   );
 };
