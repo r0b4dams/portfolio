fc81162ee0f30574f13a6360ebba1f4a8175d415
Robert Adams <adamsiii.robert@gmail.com>
2022-05-09 16:20:33 -0700

    added 2xl size override

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,15 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      screens: {
+        '2xl': '1280px',
+      },
+    },
+  },
 };
