2bf81a3a95cc6c6b23dac4bc3691a464431fe74e
Robert Adams <adamsiii.robert@gmail.com>
2022-05-09 21:13:39 -0700

    updated footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-// import Icon from '../components/Icon';
-
 const year = new Date().getFullYear();
 
 const Footer: React.FC = (): JSX.Element => {
   return (
-    <footer className='bg-white h-[5vh] border-t-4 border-blue-500'>
+    <footer className='bg-white h-[5vh] border-t-4 border-blue-500 flex flex-col items-center justify-center'>
       <div>Robert Adams ©{year}</div>
     </footer>
   );
