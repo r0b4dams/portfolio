47fa3f4be67bc24224c9caef840324644ff51dd5
Robert Adams <adamsiii.robert@gmail.com>
2022-05-09 18:55:16 -0700

    updated with intrinsic img, added borders

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,13 +4,14 @@ import Link from 'next/link';
 import { Project } from '../@types/projects';
 
 const Project: React.FC<{ project: Project }> = ({ project }) => (
-  <div className='container w-full lg:w-1/2'>
+  <div className='w-fit p-1 m-1 border-2 border-dashed border-blue-500 rounded-md shadow-lg'>
     <Link href={`/projects/${project.id}`}>
-      <a>
+      <a className='flex'>
         <Image
+          className='rounded-lg shadow'
           src={`/images/${project.id}-demo.gif`}
           alt=''
-          layout='responsive'
+          layout='intrinsic'
           height={360}
           width={640}
         />
