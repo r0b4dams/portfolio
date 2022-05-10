d4437b42d8e95ee02d9bf35c60ee9f7343324dc3
Robert Adams <adamsiii.robert@gmail.com>
2022-05-09 21:13:21 -0700

    renamed portfolio to projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -14,17 +14,21 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-const Portfolio: NextPage<{ projects: ProjectData }> = ({ projects }) => {
+const Projects: NextPage<{ projects: ProjectData }> = ({ projects }) => {
   return (
     <>
       <Head>
         <title>{'Robert Adams | Portfolio'}</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
+      <section className='container'>
+        <h1 className='w-[90%] m-auto my-5 border-b-2 border-slate-900 border-dotted p-2'>
+          Projects
+        </h1>
+      </section>
       <ProjectContainer projects={projects} />
     </>
   );
 };
 
-export default Portfolio;
+export default Projects;
