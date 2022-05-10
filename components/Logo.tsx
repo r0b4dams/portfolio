79e21b8ed50184d30cf3ea81b644115f1d485694
Robert Adams <adamsiii.robert@gmail.com>
2022-05-09 21:13:58 -0700

    tweaked breakpoint

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Logo: React.FC = () => (
   <Link href='/'>
-    <a className='h-[50px] px-[30px] relative md:h-[100px]'>
+    <a className='h-[50px] px-[30px] relative lg:h-[100px]'>
       <Image src='/icons/logo.svg' alt='' layout='fill' />
     </a>
   </Link>
