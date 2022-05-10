cebdd38f1339b53d8913068de53bf2558bff7a95
Robert Adams <adamsiii.robert@gmail.com>
2022-05-09 18:49:48 -0700

    added rule to increase size on desktop

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Logo: React.FC = () => (
   <Link href='/'>
-    <a className='h-[50px] w-[50px] px-[30px] relative'>
+    <a className='h-[50px] px-[30px] relative md:h-[100px]'>
       <Image src='/icons/logo.svg' alt='' layout='fill' />
     </a>
   </Link>
