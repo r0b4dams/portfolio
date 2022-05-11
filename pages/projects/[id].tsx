be6a4b1bae0ef3381ef1317424d17d8ca9264f08
Robert Adams <adamsiii.robert@gmail.com>
2022-05-11 10:36:59 -0700

    updated layout

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
-import Image from 'next/image';
 import { getProjectIds, getProjectData } from '../../lib/projects';
 import type { GetStaticProps, GetStaticPaths, NextPage } from 'next/types';
 import type { Project } from '../../@types/projects';
-import Icon from '../../components/Icon';
+import ProjectImage from '../../components/ProjectImage';
+import ProjectContent from '../../components/ProjectContent';
+import { getStyles } from '../../utils';
 
 export const getStaticPaths: GetStaticPaths = () => {
   const paths = getProjectIds();
@@ -22,41 +22,57 @@ export const getStaticProps: GetStaticProps = ({ params }) => {
   };
 };
 
+const headerStyles = getStyles({
+  width: 'w-[90%] md:w-full',
+  padding: 'p-2 ',
+  margin: 'mx-auto mt-5',
+  border: 'border-b-2 border-gray-100',
+  fontSize: 'text-3xl sm:text-4xl md:text-6xl',
+  fontWeight: 'font-bold',
+});
+
 const ProjectPage: NextPage<{ project: Project }> = ({ project }) => {
   return (
-    <section className='container m-auto flex flex-col items-center'>
-      <div>
-        <Image
-          src={`/images/${project.id}-demo.gif`}
-          alt=''
-          height={360}
-          width={640}
-        />
-      </div>
-      <h1>{project.name}</h1>
-      <p>{project.desc}</p>
-      <h2>Role</h2>
-      {project.role}
-      <h2>Tech</h2>
-      <p>{project.tech}</p>
-      <div>
-        <ul>
-          <li>
-            <a href={project.appURL} target='_blank' rel='noreferrer'>
-              Deployed App <Icon src={'/icons/deploy.svg'} className={''} />
-            </a>
-          </li>
-          <li>
-            <a href={project.repoURL} target='_blank' rel='noreferrer'>
-              GitHub Repo <Icon src={'/icons/github.svg'} className={''} />
-            </a>
-          </li>
-        </ul>
-        <Link href='/portfolio'>
-          <a>{'<< Back to Portfolio'}</a>
-        </Link>
-      </div>
-    </section>
+    <>
+      <section className='container'>
+        <div className='h-[90vh]'>
+          <h1 className={headerStyles}>{project.name}</h1>
+
+          <p className='w-[90%] mx-auto border-b-2 border-gray-100 p-2 lg:w-1/2 lg:m-0'>
+            {project.desc}
+          </p>
+
+          <div className='flex flex-col items-center lg:flex-row lg:flex-wrap'>
+            <ProjectImage source={`/images/${project.id}-demo.gif`} />
+            <ProjectContent heading={'Features'} />
+          </div>
+        </div>
+
+        <div
+          id='tech'
+          className='h-screen bg-gray-100 flex flex-col items-center justify-center lg:flex-row-reverse lg:flex-wrap'
+        >
+          <ProjectImage source={`/images/${project.id}-demo.gif`} />
+          <ProjectContent heading={'Technologies'} />
+        </div>
+
+        <div
+          id='role'
+          className='h-screen bg-gray-50 flex flex-col items-center justify-center lg:flex-row lg:flex-wrap'
+        >
+          <ProjectImage source={`/images/${project.id}-demo.gif`} />
+          <ProjectContent heading={'My Role'} />
+        </div>
+
+        <div
+          id='reflection'
+          className='h-[95vh] flex flex-col justify-center items-center'
+        >
+          <ProjectContent heading={'Challenges'} />
+          <ProjectContent heading={'Successes'} />
+        </div>
+      </section>
+    </>
   );
 };
 
