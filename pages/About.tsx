3c6bf857a363cb5f13c696c3977eb4cb990ee7b8
Robert Adams <adamsiii.robert@gmail.com>
2022-05-11 10:37:35 -0700

    rollback, update header styles

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,6 +1,15 @@
-import type { NextPage } from 'next/types';
-import Image from 'next/image';
 import Head from 'next/head';
+import type { NextPage } from 'next/types';
+import { getStyles } from '../utils';
+
+const headerStyles = getStyles({
+  width: 'w-[90%] md:w-full',
+  padding: 'p-2 ',
+  margin: 'm-auto my-5',
+  border: 'border-b-2 border-gray-100',
+  fontSize: 'text-3xl sm:text-4xl md:text-6xl',
+  fontWeight: 'font-bold',
+});
 
 const About: NextPage = () => {
   return (
@@ -10,35 +19,8 @@ const About: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <section id='about'>
-        <Image src='/images/avatar.jpg' alt='avatar' height={144} width={144} />
-        <h1>ABOUT</h1>
-        <br />
-        <p>
-          Coming out of college I accepted a position as an avionics repair
-          technician. It was a promising job and my adaptability and ability to
-          learn quickly paid off: in less than a year I had earned trainer
-          status in both the repair of in-flight hardware as well as the
-          maintenance repair software used for documentation.
-        </p>
-        <br />
-        <p>
-          Unfortunately, the uncertainty caused by the pandemic led to a lot of
-          restructuring and I felt like I’d hit a wall in my professional
-          growth. I realized I needed to make a change. I’d always enjoyed the
-          programming aspects of my physics degree, so when I saw a chance to
-          apply for a full stack web development certificate, I jumped at it.
-        </p>
-        <br />
-        <p>
-          I’m glad I did, because in earning it, it really solidified my
-          appreciation for what I enjoy in a job: learning new things, problem
-          solving, and helping build something tangible. I’m eager to contribute
-          my skills to a collaborative environment, and look to find a position
-          where I can further develop current skills as well as acquire new
-          ones. I’m really excited and I look forward to learning more!
-        </p>
-        <br />
+      <section className='container'>
+        <h1 className={headerStyles}>About</h1>
       </section>
     </>
   );
--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,6 +1,15 @@
 import Head from 'next/head';
 import type { NextPage } from 'next/types';
-import ContactCard from '../components/ContactCard';
+import { getStyles } from '../utils';
+
+const headerStyles = getStyles({
+  width: 'w-[90%] md:w-full',
+  padding: 'p-2 ',
+  margin: 'm-auto my-5',
+  border: 'border-b-2 border-gray-100',
+  fontSize: 'text-3xl sm:text-4xl md:text-6xl',
+  fontWeight: 'font-bold',
+});
 
 const Contact: NextPage = () => {
   return (
@@ -10,17 +19,8 @@ const Contact: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <section className='h-[40vh] p-[10px]'>
-        <h1 className='text-5xl'>{"Let's get in touch!"}</h1>
-        <p className='text-xl'>
-          Contact me for professional opportunities or project collaboration!
-        </p>
-      </section>
-      <section className='container flex flex-wrap items-center justify-around'>
-        <ContactCard src={'/icons/email.svg'} cardColor={'bg-slate-500'} />
-        <ContactCard src={'/icons/linkedin.svg'} cardColor={'bg-slate-600'} />
-        <ContactCard src={'/icons/github.svg'} cardColor={'bg-slate-700'} />
-        <ContactCard src={'/icons/resume.svg'} cardColor={'bg-slate-800'} />
+      <section className='container'>
+        <h1 className={headerStyles}>Contact</h1>
       </section>
     </>
   );
