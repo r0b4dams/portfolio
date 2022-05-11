05c0bcdc1fd78bd1ea9be27c3eb4a0a31c5a7f7a
Robert Adams <adamsiii.robert@gmail.com>
2022-05-10 23:37:58 -0700

    added icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,25 @@
+import Icon from './Icon';
+import icons from '../lib/icons';
+
 const year = new Date().getFullYear();
 
 const Footer: React.FC = (): JSX.Element => {
   return (
-    <footer className='bg-white h-[5vh] border-t-4 border-blue-500 flex flex-col items-center justify-center'>
-      <div>Robert Adams ©{year}</div>
+    <footer className='bg-white border-t-4 border-blue-500 flex flex-col items-center justify-center p-5'>
+      <div className='flex flex-col items-center space-y-5'>
+        <ul className='flex space-x-10'>
+          <li className=''>
+            <Icon icon={'gmail'} size={32} />
+          </li>
+          <li>
+            <Icon icon={'linkedin'} size={32} />
+          </li>
+          <li>
+            <Icon icon={'github'} size={32} />
+          </li>
+        </ul>
+        <div className='font-semibold'>Robert Adams © {year}</div>
+      </div>
     </footer>
   );
 };
