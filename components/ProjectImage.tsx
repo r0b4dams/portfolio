014ea04f5540d3610dd577ac83b477de384bd09c
Robert Adams <adamsiii.robert@gmail.com>
2022-05-11 10:39:58 -0700

    wrapper comp to display images on project page

---
+++ components/ProjectImage.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+const ProjectImage: React.FC<{ source: string }> = ({
+  source,
+}): JSX.Element => {
+  return (
+    <div className='w-full md:w-1/2 p-2'>
+      <div className='flex'>
+        <Image
+          src={source}
+          alt=''
+          layout='intrinsic'
+          height={360}
+          width={640}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectImage;
