fdc2299ee80ad0a1a039c9b1e4bc590a1462f349
Robert Adams <adamsiii.robert@gmail.com>
2022-05-12 13:04:51 -0700

    added placeholder content

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -4,6 +4,7 @@ import type { Project } from '../../@types/projects';
 import ProjectImage from '../../components/ProjectImage';
 import ProjectContent from '../../components/ProjectContent';
 import { getStyles } from '../../utils';
+import Icon from '../../components/Icon';
 
 export const getStaticPaths: GetStaticPaths = () => {
   const paths = getProjectIds();
@@ -24,7 +25,7 @@ export const getStaticProps: GetStaticProps = ({ params }) => {
 
 const headerStyles = getStyles({
   width: 'w-[90%] md:w-full',
-  padding: 'p-2 ',
+  padding: 'pl-10 ',
   margin: 'mx-auto mt-5',
   border: 'border-b-2 border-gray-100',
   fontSize: 'text-3xl sm:text-4xl md:text-6xl',
@@ -35,41 +36,92 @@ const ProjectPage: NextPage<{ project: Project }> = ({ project }) => {
   return (
     <>
       <section className='container'>
-        <div className='h-[90vh]'>
-          <h1 className={headerStyles}>{project.name}</h1>
+        <h1 className={headerStyles}>{project.name}</h1>
 
-          <p className='w-[90%] mx-auto border-b-2 border-gray-100 p-2 lg:w-1/2 lg:m-0'>
-            {project.desc}
-          </p>
+        <p className='w-[90%] mx-auto border-b-2 border-gray-100 p-2 lg:w-1/2 lg:m-0'>
+          {project.desc}
+        </p>
 
-          <div className='flex flex-col items-center lg:flex-row lg:flex-wrap'>
+        <div className='space-y-5'>
+          <div className='flex flex-col md:flex-row md:flex-wrap'>
             <ProjectImage source={`/images/${project.id}-demo.gif`} />
-            <ProjectContent heading={'Features'} />
+
+            <div className='w-full flex flex-col justify-center md:w-1/2 p-5 space-y-5 text-sm sm:text-base md:text:xl lg:text-xl'>
+              <p>Harum deserunt iusto aliquid accusamus dignissimos autem</p>
+
+              <p>
+                Delectus velit veniam, harum debitis quam fugiat iure, magni,
+                vitae veritatis voluptatibus asperiores alias nesciunt at!
+              </p>
+
+              <p>
+                Quia dolor aliquid eveniet aliquam, obcaecati nulla deleniti
+                impedit cum, quis ea distinctio.
+              </p>
+            </div>
           </div>
         </div>
 
-        <div
-          id='tech'
-          className='h-screen bg-gray-100 flex flex-col items-center justify-center lg:flex-row-reverse lg:flex-wrap'
-        >
+        <div className=' flex flex-col justify-center md:flex-row-reverse md:flex-wrap'>
           <ProjectImage source={`/images/${project.id}-demo.gif`} />
-          <ProjectContent heading={'Technologies'} />
+          <div className='w-full md:w-1/2 p-10 bg-gray-100'>
+            <h2 className='mb-3 text-2xl font-bold'>Technologies</h2>
+            <div className='flex flex-wrap space-x-5 p-5'>
+              {project.tech.stack.map((icon) => (
+                <Icon key={icon} size={48} icon={icon} />
+              ))}
+            </div>
+          </div>
         </div>
 
-        <div
-          id='role'
-          className='h-screen bg-gray-50 flex flex-col items-center justify-center lg:flex-row lg:flex-wrap'
-        >
+        <div className='flex flex-col justify-center md:flex-row md:flex-wrap'>
           <ProjectImage source={`/images/${project.id}-demo.gif`} />
-          <ProjectContent heading={'My Role'} />
+          <div className='w-full md:w-1/2 p-10 bg-gray-50'>
+            <h2 className='mb-3 text-2xl font-bold'>My Role</h2>
+
+            <div className='p-5 space-y-5 text-sm sm:text-base md:text:xl lg:text-xl'>
+              <p>
+                Quia dolor aliquid eveniet aliquam, obcaecati nulla deleniti
+                impedit cum, quis ea distinctio.
+              </p>
+              <p>Harum deserunt iusto aliquid accusamus dignissimos autem</p>
+              <p>
+                Delectus velit veniam, harum debitis quam fugiat iure, magni,
+                vitae veritatis voluptatibus asperiores alias nesciunt at!
+              </p>
+            </div>
+          </div>
         </div>
 
-        <div
-          id='reflection'
-          className='h-[95vh] flex flex-col justify-center items-center'
-        >
-          <ProjectContent heading={'Challenges'} />
-          <ProjectContent heading={'Successes'} />
+        <div className='flex flex-wrap'>
+          <div className='w-full md:w-1/2 p-10'>
+            <h2 className='mb-3 text-2xl font-bold'>Challenges</h2>
+            <div className='space-y-2 p-2'>
+              <p>Harum deserunt iusto aliquid accusamus dignissimos autem</p>
+              <p>
+                Delectus velit veniam, harum debitis quam fugiat iure, magni,
+                vitae veritatis voluptatibus asperiores alias nesciunt at!
+              </p>
+              <p>
+                Quia dolor aliquid eveniet aliquam, obcaecati nulla deleniti
+                impedit cum, quis ea distinctio.
+              </p>
+            </div>
+          </div>
+          <div className='w-full md:w-1/2 p-10'>
+            <h2 className='mb-3 text-2xl font-bold'>Successes</h2>
+            <div className='space-y-2 p-2'>
+              <p>
+                Delectus velit veniam, harum debitis quam fugiat iure, magni,
+                vitae veritatis voluptatibus asperiores alias nesciunt at!
+              </p>
+              <p>
+                Quia dolor aliquid eveniet aliquam, obcaecati nulla deleniti
+                impedit cum, quis ea distinctio.
+              </p>
+              <p>Harum deserunt iusto aliquid accusamus dignissimos autem</p>
+            </div>
+          </div>
         </div>
       </section>
     </>
