1143054080be6f2d83c2a72cf3132538ec57b59c
Robert Adams <adamsiii.robert@gmail.com>
2022-05-12 14:00:30 -0700

    added icons

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import type { NextPage } from 'next/types';
+import Icon from '../components/Icon';
 import { getStyles } from '../utils';
 
 const headerStyles = getStyles({
   width: 'w-[90%] md:w-full',
-  padding: 'p-2 ',
+  padding: 'pl-10 ',
   margin: 'm-auto my-5',
   border: 'border-b-2 border-gray-100',
   fontSize: 'text-3xl sm:text-4xl md:text-6xl',
@@ -21,6 +22,21 @@ const Contact: NextPage = () => {
 
       <section className='container'>
         <h1 className={headerStyles}>Contact</h1>
+        <div className='flex flex-col items-center space-y-5'>
+          <div className=''>
+            <ul className='flex space-x-10'>
+              <li>
+                <Icon icon={'gmail'} size={64} />
+              </li>
+              <li>
+                <Icon icon={'linkedin'} size={64} />
+              </li>
+              <li>
+                <Icon icon={'github'} size={64} />
+              </li>
+            </ul>
+          </div>
+        </div>
       </section>
     </>
   );
