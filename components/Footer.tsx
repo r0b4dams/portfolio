0608c5a46e515babcd603a530df37f2009e3b1d6
Robert Adams <adamsiii.robert@gmail.com>
2022-05-12 13:55:52 -0700

    removed icons, modified border

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,24 @@
-import Icon from './Icon';
-import icons from '../lib/icons';
+import Link from 'next/link';
 
 const year = new Date().getFullYear();
 
 const Footer: React.FC = (): JSX.Element => {
   return (
-    <footer className='bg-white border-t-4 border-blue-500 flex flex-col items-center justify-center p-5'>
-      <div className='flex flex-col items-center space-y-5'>
-        <ul className='flex space-x-10'>
-          <li className=''>
-            <Icon icon={'gmail'} size={32} />
-          </li>
-          <li>
-            <Icon icon={'linkedin'} size={32} />
-          </li>
-          <li>
-            <Icon icon={'github'} size={32} />
-          </li>
-        </ul>
-        <div className='font-semibold'>Robert Adams © {year}</div>
-      </div>
-    </footer>
+    <>
+      <footer className='flex flex-col items-end'>
+        <div className='h-1 bg-yellow-200 w-full'></div>
+        {/* <div className='h-1 bg-red-500 w-2/3'></div>
+        <div className='h-1 bg-blue-500 w-full'></div> */}
+
+        <div className='self-center p-5'>
+          <Link href='/contact'>
+            <a>
+              <div className='font-semibold'>Robert Adams © {year}</div>
+            </a>
+          </Link>
+        </div>
+      </footer>
+    </>
   );
 };
 
