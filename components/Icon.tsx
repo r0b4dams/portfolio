c0194dce68de13d86000b69a6e0b1cbe72fd0faa
Robert Adams <adamsiii.robert@gmail.com>
2022-05-12 13:04:16 -0700

    updated Icon component to map mulitiple paths

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -3,32 +3,29 @@ import icons from '../lib/icons';
 
 const Icon: React.FC<{
   size?: number;
-  color?: string;
-  icon: string; // name of icon
+  icon: string;
   className?: string;
   style?: CSSProperties;
-  viewBox?: string;
-}> = ({
-  size = 16,
-  icon = '',
-  color,
-  className = '',
-  style = {},
-  viewBox = '0 0 24 24',
-}) => {
-  const data = icons[icon];
+}> = ({ icon = '', size = 16, className = '', style = {} }) => {
+  const { name, paths, fills, url } = icons[icon];
   return (
-    <svg
-      className={className}
-      style={style}
-      viewBox={viewBox}
-      width={`${size}px`}
-      height={`${size}px`}
-      xmlns='http://www.w3.org/2000/svg'
-      xmlnsXlink='http://www.w3.org/1999/xlink'
-    >
-      <path fill={color ? color : data.color} d={data.path} />
-    </svg>
+    <div className={`flex items-center space-x-1`}>
+      <svg
+        role='img'
+        className={className}
+        style={style}
+        viewBox='0 0 24 24'
+        width={`${size}px`}
+        height={`${size}px`}
+        xmlns='http://www.w3.org/2000/svg'
+        xmlnsXlink='http://www.w3.org/1999/xlink'
+      >
+        <title>{name}</title>
+        {paths.map((path, idx) => (
+          <path key={idx} d={path} fill={fills[idx]}></path>
+        ))}
+      </svg>
+    </div>
   );
 };
 
