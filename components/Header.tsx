eb64cec7ccdd1feba981f2b0826c83312e4b92ac
Robert Adams <adamsiii.robert@gmail.com>
2022-05-12 14:05:48 -0700

    testing border animations

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,19 @@
+import { useRouter } from 'next/router';
 import Nav from './Nav';
 import { getStyles } from '../utils';
 
-const headerStyles = getStyles({
-  height: 'h-[10vh]',
-  background: 'bg-white',
-  border: 'border-b-4 border-blue-500',
-});
-
 const Header: React.FC = (): JSX.Element => {
+  const router = useRouter();
+  console.log(router.route);
   return (
-    <header className={headerStyles}>
-      <Nav />
-    </header>
+    <>
+      <header>
+        <Nav />
+        <div className='h-1 bg-blue-500 w-full'></div>
+        <div className='h-1 bg-red-500 w-2/3'></div>
+        <div className='h-1 bg-yellow-200 w-1/3'></div>
+      </header>
+    </>
   );
 };
 
