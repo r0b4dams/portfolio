b934fedd5678f8876b41e6167094290eb4572f11
Robert Adams <adamsiii.robert@gmail.com>
2022-05-13 13:56:09 -0700

    reverted to inline classes

--- components/ProjectContainer.tsx
+++ components/ProjectContainer.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import Project from './Project';
 import { ProjectData } from '../@types/projects';
 
-const styles = {
-  flex: 'flex flex-col items-center justify-center lg:flex-row lg:flex-wrap',
-};
-
 const ProjectContainer: React.FC<{ projects: ProjectData }> = ({
   projects,
 }): JSX.Element => {
   return (
-    <div id='portfolio' className={`${styles.flex}`}>
+    <div
+      id='portfolio'
+      className='flex flex-col items-center justify-center lg:flex-row lg:flex-wrap'
+    >
       {projects.map((project) => (
         <Project key={project.id} project={project} />
       ))}
