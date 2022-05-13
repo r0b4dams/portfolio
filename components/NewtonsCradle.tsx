8e842bab0a1e69446f674b88cd137fd3c34294fa
Robert Adams <adamsiii.robert@gmail.com>
2022-05-12 19:51:22 -0700

    first draft

---
+++ components/NewtonsCradle.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NewtonsCradle() {
+  return (
+    <div className='h-[125px] w-[300px] border-t-[22px] border-red-500 flex items-center justify-around '>
+      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full animate-left-swing origin-top'></div>
+      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full'></div>
+      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full'></div>
+      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full'></div>
+      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full animate-right-swing origin-top'></div>
+    </div>
+  );
+}
