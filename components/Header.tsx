36b12d0f43328406ceec203f5e41561bfea51f37
Robert Adams <adamsiii.robert@gmail.com>
2022-05-13 15:13:25 -0700

    added spacing to border divs

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,11 @@ const Header: React.FC = (): JSX.Element => {
     <>
       <header>
         <Nav />
-        <div className='h-1 bg-blue-500 w-full'></div>
-        <div className='h-1 bg-red-500 w-2/3'></div>
-        <div className='h-1 bg-yellow-200 w-1/3'></div>
+        <div className='space-y-1'>
+          <div className='h-1 bg-blue-500 w-full'></div>
+          <div className='h-1 bg-red-500 w-2/3'></div>
+          <div className='h-1 bg-yellow-200 w-1/3'></div>
+        </div>
       </header>
     </>
   );
