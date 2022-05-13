abab6529cdf0126f1bc5732561bd03575b44c564
Robert Adams <adamsiii.robert@gmail.com>
2022-05-13 14:02:22 -0700

    reverted to static color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,25 +4,9 @@ import { useRouter } from 'next/router';
 const year = new Date().getFullYear();
 
 const Footer: React.FC = (): JSX.Element => {
-  const router = useRouter();
-  console.log(router.route);
-
-  const color = (() => {
-    switch (router.route) {
-      case '/projects':
-        return 'bg-yellow-200';
-      case '/about':
-        return 'bg-red-500';
-      case '/contact':
-        return 'bg-blue-500';
-      default:
-        return 'bg-yellow-200';
-    }
-  })();
-
   return (
     <>
-      <div className={`h-1 w-full ${color}`}></div>
+      <div className='h-1 w-full bg-blue-500'></div>
       <footer className='flex flex-col items-end'>
         <div className='self-center p-5'>
           <Link href='/contact'>
