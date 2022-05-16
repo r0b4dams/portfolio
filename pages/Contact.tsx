a66f5f108ea27479c02796775b4b7eaf9e9edb86
Robert Adams <adamsiii.robert@gmail.com>
2022-05-15 21:08:44 -0700

    tweaked header font sizes

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -6,7 +6,7 @@ import { getStyles } from '../utils';
 
 const headerStyles = getStyles({
   margin: 'my-5',
-  fontSize: 'text-3xl sm:text-4xl md:text-6xl',
+  fontSize: 'text-3xl sm:text-5xl md:text-7xl',
   fontWeight: 'font-bold',
 });
 
@@ -20,6 +20,7 @@ const Contact: NextPage = () => {
 
       <section className='container'>
         <h1 className={headerStyles}>Contact</h1>
+
         <div className='flex flex-col items-center justify-center space-y-5'>
           <ul className='flex space-x-10'>
             <li>
