e42428fa638ab88a4e912790faeb37f7da72ce27
Robert Adams <adamsiii.robert@gmail.com>
2022-05-16 21:26:47 -0700

    testing new logo idea

---
+++ components/NewLogo.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+export default function NewLogo() {
+  return (
+    <div className='svg-wrapper'>
+      <svg
+        id='logo'
+        width='24.0px'
+        height='24.0px'
+        viewBox='0 0 24.0 24.0'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          fill='none'
+          stroke='#000000'
+          strokeWidth='2'
+          d='M 3.7298398,3.7808481 19.92984,22.824804'
+          id='yellow-shadow'
+          className='yellow'
+        />
+
+        <path
+          fill='none'
+          stroke='#000000'
+          strokeWidth='2'
+          d='m 2.4,2.5 h 6.5 c 2.643593,2e-7 4.624355,1.1435937 5.696152,3 1.071797,1.8564065 1.071797,4.1435935 0,6 -1.071797,1.856406 -3.052559,3 -5.696152,3 H 7.4'
+          id='blue-shadow'
+          className='blue'
+        />
+
+        <path
+          fill='none'
+          stroke='#000000'
+          strokeWidth='2'
+          d='m 3.4,1.5 v 22'
+          id='red-shadow'
+          className='red'
+        />
+
+        <path
+          fill='none'
+          stroke='#ffff00'
+          strokeWidth='2'
+          d='M 3.8,3.15 20,22'
+          id='yellow'
+          className='yellow'
+        />
+
+        <path
+          fill='none'
+          stroke='#0000ff'
+          strokeWidth='2'
+          d='m 3,2 h 6.5 c 2.643593,2e-7 4.624355,1.1435937 5.696152,3 1.071797,1.8564065 1.071797,4.1435935 0,6 C 14.124355,12.856406 12.143593,14 9.5,14 H 8'
+          id='blue'
+          className='blue'
+        />
+
+        <path
+          fill='none'
+          stroke='#ff0000'
+          strokeWidth='2'
+          d='M 4,1 V 23'
+          id='red'
+          className='red'
+        />
+      </svg>
+    </div>
+  );
+}
