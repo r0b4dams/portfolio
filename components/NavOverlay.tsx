e9b1a99bbd6040431d1231eca9b538104e27dcb2
Robert Adams <adamsiii.robert@gmail.com>
2022-05-17 14:54:42 -0700

    fixed overlay

--- components/NavOverlay.tsx
+++ components/NavOverlay.tsx
@@ -1,14 +1,38 @@
-import NavList from './NavList';
+import Link from 'next/link';
 
 type MenuProps = {
   active: boolean;
-  toggleActive: () => void;
+  handleToggle: () => void;
 };
 
-const NavOverlay: React.FC<MenuProps> = ({ active, toggleActive }) => (
-  <div className='hidden'>
-    <NavList />
-  </div>
-);
+const NavOverlay: React.FC<MenuProps> = ({ active, handleToggle }) => {
+  return (
+    <nav
+      className={
+        active
+          ? 'overlay flex justify-center items-center translate-x-0'
+          : 'overlay flex justify-center items-center'
+      }
+    >
+      <ul className='font-bold text-3xl space-y-10'>
+        <li>
+          <Link href='/projects'>
+            <a>Projects</a>
+          </Link>
+        </li>
+        <li className=''>
+          <Link href='/about'>
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/contact'>
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavOverlay;
