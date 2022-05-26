347ca270de80cc5017b99eb674f433782521b78f
Robert Adams <adamsiii.robert@gmail.com>
2022-05-25 19:13:34 -0700

    rebuilt cradle with flex to fix resize issue

--- components/NewtonsCradle.tsx
+++ components/NewtonsCradle.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-
-export default function NewtonsCradle() {
+const NewtonsCradle = () => {
   return (
-    <div className='h-[125px] w-[300px] border-t-[22px] border-red-500 flex items-center justify-around '>
-      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full animate-left-swing origin-top'></div>
-      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full'></div>
-      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full'></div>
-      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full'></div>
-      <div className='-z-10 h-[125px] w-[4px] bg-yellow-300 relative rounded-sm after:absolute after:h-[60px] after:w-[60px] after-[60px] after:bg-blue-500 after:left-[-28px] after:bottom-[-60px] after:rounded-full animate-right-swing origin-top'></div>
+    <div className='flex flex-col items-center justify-evenly'>
+      <div className='cradle'>
+        <div className='bg-red-500 h-5 rounded w-full'></div>
+        <div className='w-[200px] flex justify-evenly border-top md:w-[300px]'>
+          <div className='w-1/5 flex flex-col justify-center items-center animate-left-swing origin-top'>
+            <div className='bg-yellow-300 w-1 h-20 md:h-32'></div>
+            <div className='bg-blue-600 w-full aspect-square rounded-full'></div>
+          </div>
+          <div className='w-1/5 flex flex-col justify-center items-center'>
+            <div className='bg-yellow-300 w-1 h-20 md:h-32'></div>
+            <div className='bg-blue-600 w-full aspect-square rounded-full'></div>
+          </div>
+          <div className='w-1/5 flex flex-col justify-center items-center'>
+            <div className='bg-yellow-300 w-1 h-20 md:h-32'></div>
+            <div className='bg-blue-600 w-full aspect-square rounded-full'></div>
+          </div>
+          <div className='w-1/5 flex flex-col justify-center items-center'>
+            <div className='bg-yellow-300 w-1 h-20 md:h-32'></div>
+            <div className='bg-blue-600 w-full aspect-square rounded-full'></div>
+          </div>
+          <div className='w-1/5 flex flex-col justify-center items-center animate-right-swing origin-top'>
+            <div className='bg-yellow-300 w-1 h-20 md:h-32'></div>
+            <div className='bg-blue-600 w-full aspect-square rounded-full'></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default NewtonsCradle;
