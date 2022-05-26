5500a5c6b99fe03655a4ace12829ba472252251d
Robert Adams <adamsiii.robert@gmail.com>
2022-05-25 19:11:27 -0700

    temp removed borders

--- components/NavList.tsx
+++ components/NavList.tsx
@@ -4,17 +4,17 @@ const NavList: React.FC = (): JSX.Element => {
   return (
     <nav className='hidden md:block'>
       <ul className='flex space-x-12 font-semibold text-lg'>
-        <li className='border-b-2 border-yellow-500'>
+        <li className=''>
           <Link href='/projects'>
             <a>Projects</a>
           </Link>
         </li>
-        <li className='border-b-2 border-red-500'>
+        <li className=''>
           <Link href='/about'>
             <a>About</a>
           </Link>
         </li>
-        <li className='border-b-2 border-blue-500'>
+        <li className=''>
           <Link href='/contact'>
             <a>Contact</a>
           </Link>
