7b4f8df1336e0925d2427e2c95fb960e1375573d
Robert Adams <adamsiii.robert@gmail.com>
2022-05-25 19:14:17 -0700

    simplified styling

--- components/ProjectImage.tsx
+++ components/ProjectImage.tsx
@@ -4,16 +4,8 @@ const ProjectImage: React.FC<{ source: string }> = ({
   source,
 }): JSX.Element => {
   return (
-    <div className='w-full md:w-1/2 p-1'>
-      <div className='flex'>
-        <Image
-          src={source}
-          alt=''
-          layout='intrinsic'
-          height={360}
-          width={640}
-        />
-      </div>
+    <div className='flex'>
+      <Image src={source} alt='' layout='intrinsic' height={360} width={640} />
     </div>
   );
 };
