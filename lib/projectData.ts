972f4e25669c850d3cfbc55292fe33d2371d8e67
Robert Adams <adamsiii.robert@gmail.com>
2022-05-25 19:16:23 -0700

    added features content, outlined discussion

--- lib/projectData.ts
+++ lib/projectData.ts
@@ -7,33 +7,29 @@ const projectData = [
     repoURL: 'https://github.com/comatosino/Traductora',
     imgsrc: '/images/traductora-demo.gif',
     features: [
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
+      'Speak in one language and hear its translation in another!',
+      'Utilizes JSON Web Tokens (JWTs) for authentication and authorization.',
+      'Custom React hooks interface with the browser Web Speech API Speech-to-Text (SpeechRecognition) and Text-to-Speech (SpeechSynthesis) interfaces.',
     ],
     tech: ['react', 'node', 'express', 'mongodb', 'redux', 'mui'],
-    challenges: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
-    ],
-    successes: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
+    discussion: [
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
     ],
   },
   {
     id: 'console-log',
     name: 'Console Log',
-    desc: 'Create, store, and link to guides and walkthroughs for video games.',
+    desc: 'A hub for players to store walkthroughs for their favorite video games!',
     appURL: 'https://sar-console-log.herokuapp.com/',
     repoURL: 'https://github.com/comatosino/Console-Log',
     imgsrc: '/images/console-log-demo.gif',
     features: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
+      'Create a user profile and start saving text or links to your favorite walkthroughs.',
+      'View, rate, and favorite walkthroughs posted by other users!',
+      'Search for games using voice commands!',
+      "Won “Best Presentation” of all final project submissions for University of Washington's spring full-stack web development certificate program.",
     ],
     tech: ['react', 'node', 'express', 'mongodb', 'sequelize', 'tailwind'],
     role: [
@@ -41,15 +37,10 @@ const projectData = [
       'Designed database and built RESTful API',
       'Managed GitHub repository, led code reviews, and approved pull requests',
     ],
-    challenges: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
-    ],
-    successes: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
+    discussion: [
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
     ],
   },
   {
@@ -60,24 +51,20 @@ const projectData = [
     repoURL: 'https://github.com/Gushihiro/Vedomy',
     imgsrc: '/images/vedomy-demo.gif',
     features: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
+      'Reflect on your exercise, diet, and mood. Record your thoughts.',
+      'Use the box-breathing module to return your breathing to its normal rhythm.',
+      'Check out daily yoga, sleep, and recepie suggestions.',
+      'Won "People\'s Choice" award for University of Washington\'s spring full-stack web development certificate program',
     ],
     tech: ['javascript', 'html', 'css', 'materialize'],
     role: [
       'Interfaced with 3rd-Party APIs to fetch data, created and populated recipe card',
       'Led code reviews and approved pull requests',
     ],
-    challenges: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
-    ],
-    successes: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
+    discussion: [
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
     ],
   },
   {
@@ -88,25 +75,21 @@ const projectData = [
     repoURL: 'https://github.com/comatosino/Pantrimonium',
     imgsrc: '/images/pantrimonium-demo.gif',
     features: [
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
+      'Create a user profile to get started!',
+      'Add, edit, and remove tiems from your fridge, freezer, or pantry!',
+      'Generate a shopping list from items low in quantity.',
+      'Be notified of expired items.',
     ],
     tech: ['node', 'express', 'mysql', 'bootstrap', 'handlebars'],
     role: [
-      'implemented client-side CRUD functionality',
-      'Built server framework and added e-mail component with Nodemailer',
       'Fulfilled project manager and GitHub repository master roles',
+      'Implemented client-side CRUD functionality',
+      'Built server framework and added e-mail component with Nodemailer',
     ],
-    challenges: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
-    ],
-    successes: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
+    discussion: [
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
     ],
   },
   {
@@ -117,20 +100,13 @@ const projectData = [
     repoURL: 'https://github.com/comatosino/dad-jokes',
     imgsrc: '/images/dad-jokes-demo.gif',
     features: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
+      'Mix and match rugged whiskers with wholesome punchlines!',
+      'Grab an individual joke, beard, or both!',
     ],
     tech: ['react', 'node', 'mui'],
-    challenges: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
-    ],
-    successes: [
-      'Delectus velit veniam, harum debitis quam fugiat iure, magni, vitae veritatis voluptatibus asperiores alias nesciunt at!',
-      'Harum deserunt iusto aliquid accusamus dignissimos autem',
-      'Quia dolor aliquid eveniet aliquam, obcaecati nulla delenitiimpedit cum, quis ea distinctio.',
+    discussion: [
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+      'Dadicated to Justin',
     ],
   },
 ];
