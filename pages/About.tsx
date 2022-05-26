f07809e6cd24d1578cb924b566e1e06dd8b15955
Robert Adams <adamsiii.robert@gmail.com>
2022-05-25 19:16:40 -0700

    added about summary

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import type { NextPage } from 'next/types';
-import Icon from '../components/Icon';
 import SkillBadge from '../components/SkillBadge';
 import SpanishBadge from '../components/SpanishBadge';
 import { getStyles } from '../utils';
@@ -36,21 +35,33 @@ const About: NextPage = () => {
             <h2 className='py-4 pb-1 text-xl font-bold lg:text-3xl'>
               Robert Adams
             </h2>
-            <p className='text-gray-600'>Software Engineer</p>
+            <p className='text-gray-600 pb-5'>Software Engineer</p>
           </div>
 
-          <div className='md:w-2/3'>
-            <p className='text-sm py-3 md:text-base text-gray-600'>
-              Ducimus facere similique ea quis? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Architecto alias consequuntur, nihil
-              pariatur minima cupiditate saepe laboriosam quam distinctio eos
-              totam natus in ullam nisi.
+          <div className='md:w-2/3 space-y-5'>
+            <p>
+              Coming out of college I accepted a position as an avionics repair
+              technician. It was a promising job and my adaptability and ability
+              to learn quickly paid off: in less than a year I had earned
+              trainer status in both the repair of in-flight hardware as well as
+              the maintenance repair software used for documentation.
             </p>
-            <p className='text-sm py-3 md:text-base text-gray-600'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Architecto alias consequuntur, nihil pariatur minima cupiditate
-              saepe laboriosam quam distinctio eos totam natus in ullam nisi
-              ducimus facere similique ea quis?
+            <p>
+              Unfortunately, the uncertainty caused by the pandemic led to a lot
+              of restructuring and I felt like I’d hit a wall in my professional
+              growth. I realized I needed to make a change. I’d always enjoyed
+              the programming aspects of my physics degree, so when I saw a
+              chance to apply for a full stack web development certificate, I
+              jumped at it.
+            </p>
+            <p>
+              I’m glad I did, because in earning it, it really solidified my
+              appreciation for what I enjoy in a job: learning new things,
+              problem solving, and helping build something tangible. I’m eager
+              to contribute my skills to a collaborative environment, and look
+              to find a position where I can further develop current skills as
+              well as acquire new ones. I’m really excited and I look forward to
+              learning more!
             </p>
 
             <div className='space-y-2'>
