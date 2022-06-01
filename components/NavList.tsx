b71736392e9cb08f4522b4b510bca6fe5c2fc43d
Robert Adams <adamsiii.robert@gmail.com>
2022-06-01 16:29:22 -0700

    added spring animations

--- components/NavList.tsx
+++ components/NavList.tsx
@@ -1,24 +1,49 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useTrail, useSprings, animated } from 'react-spring';
+
+const routes = ['projects', 'about', 'contact'];
+
+const colorMap: { [key: string]: string } = {
+  projects: '#0000ff',
+  about: '#ff0000',
+  contact: '#ffff00',
+};
 
 const NavList: React.FC = (): JSX.Element => {
+  const router = useRouter();
+
+  const trail = useTrail(routes.length, {
+    from: { opacity: 0, y: 20 },
+    to: { opacity: 1, y: 0 },
+  });
+
+  const routeStyle = useSprings(
+    routes.length,
+    routes.map((route) => ({
+      from: { width: '0%' },
+      to: { width: router.pathname === `/${route}` ? '100%' : '0%' },
+    }))
+  );
+
   return (
     <nav className='hidden md:block'>
       <ul className='flex space-x-12 font-semibold text-lg'>
-        <li className=''>
-          <Link href='/projects'>
-            <a>Projects</a>
-          </Link>
-        </li>
-        <li className=''>
-          <Link href='/about'>
-            <a>About</a>
-          </Link>
-        </li>
-        <li className=''>
-          <Link href='/contact'>
-            <a>Contact</a>
-          </Link>
-        </li>
+        {trail.map((mountStyle, idx) => (
+          <animated.li
+            key={routes[idx]}
+            style={mountStyle}
+            className='hover:scale-125 duration-150 origin-center first-letter:uppercase'
+          >
+            <Link href={`/${routes[idx]}`}>
+              <a>{routes[idx]}</a>
+            </Link>
+            <animated.div
+              style={routeStyle[idx]}
+              className={`h-2 bg-[${colorMap[routes[idx]]}]`}
+            ></animated.div>
+          </animated.li>
+        ))}
       </ul>
     </nav>
   );
