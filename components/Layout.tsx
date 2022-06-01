169bdf624ed9adb0146f74929598b88fc41e451d
Robert Adams <adamsiii.robert@gmail.com>
2022-06-01 12:56:12 -0700

    prettier, removed unused import

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,32 +3,30 @@ import Header from './Header';
 import Footer from './Footer';
 
 import { useTransition, animated } from 'react-spring';
-import { config } from 'process';
 
 const Layout: React.FC<{
   page: JSX.Element | JSX.Element[];
 }> = ({ page }) => {
   const transition = useTransition(page, {
-    from: {opacity: 0},
+    from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
     exitBeforeEnter: true,
   });
 
   return (
-    <>
-      <div className='flex flex-col min-h-screen'>
-        <Header />
-        {transition((styles, item) => {
-          return (
-            <animated.main style={styles} className='flex grow hide-scrollbar w-[95%] m-auto lg:w-full'>
-              {item}
-            </animated.main>
-          );
-        })}
-        <Footer />
-      </div>
-    </>
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      {transition((styles, item) => (
+        <animated.main
+          style={styles}
+          className='flex grow hide-scrollbar w-[95%] m-auto lg:w-full'
+        >
+          {item}
+        </animated.main>
+      ))}
+      <Footer />
+    </div>
   );
 };
 
