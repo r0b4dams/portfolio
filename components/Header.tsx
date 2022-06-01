ba8d7caf9af2679ca7d552208e269db8a8c46427
Robert Adams <adamsiii.robert@gmail.com>
2022-06-01 12:51:46 -0700

    added spring animations for div widths

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,57 @@
 import Nav from './Nav';
+import { useRouter } from 'next/router';
+import { useSpring, animated } from 'react-spring';
+
+const blueMap: { [key: string]: string } = {
+  '/projects': '100%',
+  '/about': '33%',
+  '/contact': '66%',
+};
+
+const redMap: { [key: string]: string } = {
+  '/projects': '66%',
+  '/about': '100%',
+  '/contact': '33%',
+};
+
+const yellowMap: { [key: string]: string } = {
+  '/projects': '33%',
+  '/about': '66%',
+  '/contact': '100%',
+};
 
 const Header: React.FC = (): JSX.Element => {
+  const router = useRouter();
+  const blueStyle = useSpring({
+    from: { width: '0%' },
+    to: { width: blueMap[router.pathname] ?? '0%' },
+  });
+  const redStyle = useSpring({
+    from: { width: '0%' },
+    to: { width: redMap[router.pathname] ?? '0%' },
+  });
+  const yellowStyle = useSpring({
+    from: { width: '0%' },
+    to: { width: yellowMap[router.pathname] ?? '0%' },
+  });
+
   return (
     <>
       <header>
         <Nav />
         <div className='space-y-1'>
-          <div className='h-1 bg-blue-500 w-full relative z-10'></div>
-          <div className='h-1 bg-red-500 w-2/3 relative z-10'></div>
-          <div className='h-1 bg-yellow-200 w-1/3 relative z-10'></div>
+          <animated.div
+            style={blueStyle}
+            className='h-2 bg-[#0000ff] relative z-10'
+          ></animated.div>
+          <animated.div
+            style={redStyle}
+            className='h-2 bg-[#ff0000] relative z-10'
+          ></animated.div>
+          <animated.div
+            style={yellowStyle}
+            className='h-2 bg-[#ffff00] relative z-10'
+          ></animated.div>
         </div>
       </header>
     </>
