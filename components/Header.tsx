6c5d2214d1e3eeb9989bad0cc2b4e2be1e986615
Robert Adams <adamsiii.robert@gmail.com>
2022-06-02 12:37:28 -0700

    factored out animated border to own component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,59 +1,13 @@
 import Nav from './Nav';
-import { useRouter } from 'next/router';
-import { useSpring, animated } from 'react-spring';
-
-const blueMap: { [key: string]: string } = {
-  '/projects': '100%',
-  '/about': '33%',
-  '/contact': '66%',
-};
-
-const redMap: { [key: string]: string } = {
-  '/projects': '66%',
-  '/about': '100%',
-  '/contact': '33%',
-};
-
-const yellowMap: { [key: string]: string } = {
-  '/projects': '33%',
-  '/about': '66%',
-  '/contact': '100%',
-};
+import HeaderBorder from './HeaderBorder';
 
 const Header: React.FC = (): JSX.Element => {
-  const router = useRouter();
-  const blueStyle = useSpring({
-    from: { width: '0%' },
-    to: { width: blueMap[router.pathname] ?? '0%' },
-  });
-  const redStyle = useSpring({
-    from: { width: '0%' },
-    to: { width: redMap[router.pathname] ?? '0%' },
-  });
-  const yellowStyle = useSpring({
-    from: { width: '0%' },
-    to: { width: yellowMap[router.pathname] ?? '0%' },
-  });
-
   return (
     <>
       <header>
         <Nav />
-        <div className='space-y-1'>
-          <animated.div
-            style={blueStyle}
-            className='h-2 bg-[#0000ff] relative z-10'
-          ></animated.div>
-          <animated.div
-            style={redStyle}
-            className='h-2 bg-[#ff0000] relative z-10'
-          ></animated.div>
-          <animated.div
-            style={yellowStyle}
-            className='h-2 bg-[#ffff00] relative z-10'
-          ></animated.div>
-        </div>
       </header>
+      <HeaderBorder />
     </>
   );
 };
