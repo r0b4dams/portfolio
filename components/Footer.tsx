27e17fc344f9ae9c89d0871b8d3a2feaf6c136ae
Robert Adams <adamsiii.robert@gmail.com>
2022-06-02 12:54:38 -0700

    link disabled on contact page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,18 +4,23 @@ import { useRouter } from 'next/router';
 const year = new Date().getFullYear();
 
 const Footer: React.FC = (): JSX.Element => {
+  const router = useRouter();
   return (
     <>
-      <footer className='flex flex-col items-end py-5'>
-        <div className='self-center'>
+      <footer className='flex flex-col items-center py-10'>
+        <div className=''>
           <Link href='/contact'>
-            <a>
-              <div className='font-semibold space-x-1'>
-                <span className='border-b-blue-500 border-b'>Robert</span>
-                <span className='border-b-red-500 border-b'>Adams</span>
-                <span>©</span>
-                <span className='border-b-yellow-200 border-b'>{year}</span>
-              </div>
+            <a
+              className={
+                router.pathname === '/contact'
+                  ? 'pointer-events-none font-semibold space-x-1'
+                  : 'font-semibold space-x-1'
+              }
+            >
+              <span className='border-b-blue-500 border-b'>Robert</span>
+              <span className='border-b-red-500 border-b'>Adams</span>
+              <span>©</span>
+              <span className='border-b-yellow-200 border-b'>{year}</span>
             </a>
           </Link>
         </div>
