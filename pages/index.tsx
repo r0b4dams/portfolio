e4b39bdea58f263addbdcd3c9f644dece14a4761
Robert Adams <adamsiii.robert@gmail.com>
2022-06-07 19:31:41 -0700

    replacede img with svg to animate paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Image from 'next/image';
+import { useSpring, animated, config } from 'react-spring';
+import { useState, useEffect } from 'react';
+import IntroSubtitle from '../components/IntroSubtitle';
+import IntroTitle from '../components/IntroTitle';
+import { Cursor } from 'react-simple-typewriter';
 
 const Intro = () => {
+  const [show, set] = useState(false);
+
+  const [{ x }, drawAPI] = useSpring(() => ({ x: 0 }));
+  useEffect(() => {
+    drawAPI.start({ x: 1, config: config.molasses });
+  }, []);
+
+  const [hideBtn, btnAPI] = useSpring(() => ({ opacity: 0, y: 20 }));
+
+  const handleLoopDone = () => {
+    set(() => true);
+    btnAPI.start({ opacity: 1, y: 0, config: config.molasses, delay: 1500 });
+  };
+
   return (
     <>
       <Head>
@@ -12,31 +30,114 @@ const Intro = () => {
 
       <div className='container flex items-center justify-center min-h-[100vh] w-[100vw]'>
         <div className='w-full md:w-1/2 flex flex-col'>
-          <div className='md:hidden self-center'>
-            <Image
-              src={`/icons/drawing-1.svg`}
-              alt=''
-              height={150}
-              width={150}
-            />
+          <div className='md:hidden self-center md:self-auto'>
+            <svg
+              role='img'
+              width={300}
+              height={300}
+              viewBox='0 0 24.0 24.0'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <g>
+                <animated.rect
+                  id='rectangle'
+                  fill='none'
+                  stroke='#ff0000'
+                  strokeWidth={0.5}
+                  strokeLinecap='round'
+                  strokeDasharray={100}
+                  strokeDashoffset={x.to((x) => (1 - x) * 100)}
+                  width='10'
+                  height='22'
+                  x='13'
+                  y='1'
+                />
+                <animated.path
+                  id='triangle'
+                  fill='none'
+                  stroke='#ffff00'
+                  strokeWidth={0.5}
+                  strokeLinecap='round'
+                  strokeDasharray={50}
+                  strokeDashoffset={x.to((x) => (1 - x) * 50)}
+                  d='M 1,23 H 13 L 7,11 Z'
+                />
+                <animated.circle
+                  id='circle'
+                  fill='none'
+                  stroke='#0000ff'
+                  strokeWidth={0.5}
+                  strokeLinecap='round'
+                  strokeDasharray={50}
+                  strokeDashoffset={x.to((x) => (1 - x) * 50)}
+                  cx='13'
+                  cy='17'
+                  r='6'
+                />
+              </g>
+            </svg>
           </div>
 
-          <div className='pl-3 '>
-            <h1 className='text-5xl my-5 md:text-8xl'>Hi, I&apos;m Robert!</h1>
-            <h2 className='text-2xl my-5 md:text-5xl'>Software Engineer</h2>
+          <div className='pl-3'>
+            <IntroTitle handleLoopDone={handleLoopDone} />
+            <div className='min-h-[50px]'>{show && <IntroSubtitle />}</div>
           </div>
 
-          <div className='my-5 self-center'>
+          <animated.div style={hideBtn} className='my-5 self-center'>
             <Link href='/projects'>
               <a className='px-5 py-2 text-l md:text-xl rounded border-2 border-gray-100'>
                 View Portfolio
               </a>
             </Link>
-          </div>
+          </animated.div>
         </div>
 
         <div className='w-full md:w-1/2 hidden md:block'>
-          <Image src={`/icons/drawing-1.svg`} alt='' height={600} width={600} />
+          <svg
+            role='img'
+            width={600}
+            height={600}
+            viewBox='0 0 24.0 24.0'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <g>
+              <animated.rect
+                id='rectangle'
+                fill='none'
+                stroke='#ff0000'
+                strokeWidth={0.5}
+                strokeLinecap='round'
+                strokeDasharray={100}
+                strokeDashoffset={x.to((x) => (1 - x) * 100)}
+                width='10'
+                height='22'
+                x='13'
+                y='1'
+              />
+              <animated.path
+                id='triangle'
+                fill='none'
+                stroke='#ffff00'
+                strokeWidth={0.5}
+                strokeLinecap='round'
+                strokeDasharray={50}
+                strokeDashoffset={x.to((x) => (1 - x) * 50)}
+                d='M 1,23 H 13 L 7,11 Z'
+              />
+              <animated.circle
+                id='circle'
+                fill='none'
+                stroke='#0000ff'
+                strokeWidth={0.5}
+                strokeLinecap='round'
+                strokeDasharray={50}
+                strokeDashoffset={x.to((x) => (1 - x) * 50)}
+                cx='13'
+                cy='17'
+                r='6'
+              />
+            </g>
+          </svg>
         </div>
       </div>
     </>
