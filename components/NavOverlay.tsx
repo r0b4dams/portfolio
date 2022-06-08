ebf8d83556ae0fe3fd4554a92cf5e64bbfdea1ed
Robert Adams <adamsiii.robert@gmail.com>
2022-06-07 20:44:34 -0700

    remade with useSpring

--- components/NavOverlay.tsx
+++ components/NavOverlay.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useSpring, animated, config, easings } from 'react-spring';
 
 type MenuProps = {
   active: boolean;
@@ -6,13 +7,14 @@ type MenuProps = {
 };
 
 const NavOverlay: React.FC<MenuProps> = ({ active, handleToggle }) => {
+  const style = useSpring({
+    to: { x: active ? '0%' : '100%' },
+  });
+
   return (
-    <nav
-      className={
-        active
-          ? 'overlay flex justify-center items-center translate-x-0'
-          : 'overlay flex justify-center items-center'
-      }
+    <animated.nav
+      style={style}
+      className='overlay flex justify-center items-center'
     >
       <ul className='font-bold text-3xl space-y-10'>
         <li>
@@ -31,7 +33,7 @@ const NavOverlay: React.FC<MenuProps> = ({ active, handleToggle }) => {
           </Link>
         </li>
       </ul>
-    </nav>
+    </animated.nav>
   );
 };
 
