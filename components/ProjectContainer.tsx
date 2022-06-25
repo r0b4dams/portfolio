59b7bb396ab29701bf937d2039e6ffef9018d384
Robert Adams <adamsiii.robert@gmail.com>
2022-06-24 20:30:21 -0700

    moved animations to Project Component

--- components/ProjectContainer.tsx
+++ components/ProjectContainer.tsx
@@ -1,21 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Project from './Project';
 import { ProjectData } from '../@types/projects';
-import { useTrail } from 'react-spring';
 
 const ProjectContainer: React.FC<{ projects: ProjectData }> = ({
   projects,
 }): JSX.Element => {
-  const [trail, api] = useTrail(projects.length, () => ({ opacity: 0, y: 25 }));
-
-  useEffect(() => {
-    api.start({ opacity: 1, y: 0 });
-  }, []);
-
   return (
     <div className='w-full flex flex-col items-center justify-center lg:flex-row lg:flex-wrap'>
-      {trail.map((style, idx) => (
-        <Project key={projects[idx].id} project={projects[idx]} style={style} />
+      {projects.map((project) => (
+        <Project key={project.id} project={project} />
       ))}
     </div>
   );
