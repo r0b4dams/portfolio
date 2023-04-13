4dc26eab6ede245a6527a8e4d73c57615a167f29
Robert Adams <adamsiii.robert@gmail.com>
2023-04-12 19:52:02 -0700

    extend config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: [
-    "./src/layouts/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "pm-blue": "#0300AD",
+        "pm-red": "#E70503",
+        "pm-yellow": "#FDDE06",
+        "pm-gray": "#EAEFE9",
+        "pm-black": "#050103",
+      },
+      screens: {
+        "2xl": "1280px",
+      },
+    },
+  },
 };
