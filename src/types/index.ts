bd19331d71d55e3ead81d8579587674c025f0ad5
Robert Adams <adamsiii.robert@gmail.com>
2023-04-14 17:26:10 -0700

    add project type

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type IProject = {
+  id: string;
+  name: string;
+  desc: string;
+  appURL: string;
+  repoURL: string;
+  imgsrc: string;
+  features: string[];
+  tech: string[];
+  role?: string[];
+  discussion: string[];
+};
