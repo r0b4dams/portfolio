4e5ec5664d2e3dd72cb0182479cda3cdb1cafd1d
Robert Adams <adamsiii.robert@gmail.com>
2023-04-14 17:26:23 -0700

    add index for exports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Page } from "./Page";
+export { Avatar } from "./Avatar";
+export { Badges } from "./Badges";
+export { ResumeSpring } from "./ResumeSpring";
+export { NewtonsCradle } from "./NewtonsCradle";
