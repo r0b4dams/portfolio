2de9acfe02b93a024b73c5dffcacfce3e58bd998
Robert Adams <adamsiii.robert@gmail.com>
2023-04-22 22:06:11 -0700

    helper

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,24 +1,18 @@
 import { icons } from "@/lib/icons";
 import { Icon } from "./Icon";
-
-const DM_SKIP = ["bash", "kubernetes"];
-
-const dm = (skill: string) => {
-  return DM_SKIP.includes(skill)
-    ? "dark:grayscale"
-    : "dark:grayscale dark:brightness-0 dark:invert";
-};
+import { filterDarkMode } from "@/utils/filterDarkMode";
 
 export const Badge: React.FC<{
+  text?: string;
   skill: string;
   size?: number;
-}> = ({ skill = "", size = 24 }) => {
+}> = ({ text, skill = "", size = 24 }) => {
   const { name, path } = icons[skill];
 
   return (
-    <div className="flex items-center w-fit bg-white bg-opacity-10 rounded p-1 mr-1 mb-1 border dark:border-slate-600">
-      <Icon src={path} size={size} className={dm(skill)} />
-      <span className="px-2 font-light text-md">{name}</span>
+    <div className="flex items-center w-fit bg-slate-50 bg-opacity-10 rounded p-1 mr-1 mb-1 border dark:border-slate-600">
+      <Icon src={path} size={size} className={filterDarkMode(skill)} />
+      <span className="px-2 font-light text-md">{text || name}</span>
     </div>
   );
 };
