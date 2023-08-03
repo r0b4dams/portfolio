868c46b6c93b95c0863cf893821314eb5fc61f93
Robert Adams <adamsiii.robert@gmail.com>
2023-08-03 14:12:18 -0700

    rename to AppLink

--- src/components/Greeting/AppLink.tsx
+++ src/components/Greeting/AppLink.tsx
@@ -1,18 +1,18 @@
-import NextLink from 'next/link';
+import Link from 'next/link';
 
 interface Props {
   href: string;
 }
 
-export const Link: React.FC<Props> = ({ href }) => {
+export const AppLink: React.FC<Props> = ({ href }) => {
   return (
     <div className='mt-10 self-center visible'>
-      <NextLink
+      <Link
         href={href}
         className='px-5 py-2 text-l md:text-xl rounded border dark:border-slate-600 bg-slate-50 bg-opacity-10'
       >
         View Portfolio
-      </NextLink>
+      </Link>
     </div>
   );
 };
