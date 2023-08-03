0731646ddc3f8f50f590b634c70dd1a73b797738
Robert Adams <adamsiii.robert@gmail.com>
2023-08-02 18:44:43 -0700

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "pm-blue": "#0300AD",
+        "pm-red": "#E70503",
+        "pm-yellow": "#FDDE06",
+        "pm-gray": "#EAEFE9",
+        "pm-black": "#050103",
+      },
+      screens: {
+        "2xl": "1280px",
       },
     },
   },
-  plugins: [],
-}
+};
