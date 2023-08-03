13e4200bb452b7b4c84a61c8d58f5548f0159783
Robert Adams <adamsiii.robert@gmail.com>
2023-08-03 14:25:10 -0700

    update comment

--- public/scripts/dark-mode.js
+++ public/scripts/dark-mode.js
@@ -1,7 +1,9 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// On page load or when changing themes, best to apply class in <head> to avoid FOUC
 // https://tailwindcss.com/docs/dark-mode
+
 const html = document.querySelector('html');
 const enabled = localStorage.getItem('dark_mode');
+
 if (enabled) {
   html?.classList.toggle('dark', JSON.parse(enabled));
 } else {
