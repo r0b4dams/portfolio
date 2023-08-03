ef3c3019989ee9d4cd0b3dd331148dbd2ced9909
Robert Adams <adamsiii.robert@gmail.com>
2023-08-03 14:25:28 -0700

    rm unused api endpoint

--- src/app/api/projects/route.ts
+++
@@ -1,7 +0,0 @@
-import { type NextRequest, NextResponse as res } from 'next/server';
-
-import projects from './projects.json';
-
-export async function GET(req: NextRequest) {
-  return res.json(projects);
-}
