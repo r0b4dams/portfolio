e3e3429c6244d49f3784e592a0191f03b41f8a49
Robert Adams <adamsiii.robert@gmail.com>
2023-08-03 10:20:21 -0700

    update config prop names

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 export const CONFIG = {
-  EMAIL: process.env.EMAIL,
-  LINKEDIN: process.env.LINKEDIN,
-  GITHUB: process.env.GITHUB,
-  RESUME: process.env.RESUME,
-  PILLAR: process.env.PILLAR,
+  EMAIL: process.env.EMAIL ?? '',
+  LINKEDIN_URL: process.env.LINKEDIN ?? '',
+  GITHUB_URL: process.env.GITHUB ?? '',
+  RESUME_URL: process.env.RESUME ?? '',
+  PILLAR_URL: process.env.PILLAR ?? '',
   FEATURE: {
     ENABLE_DEPLOYED_LINK: process.env.ENABLE_DEPLOYED_LINK === 'true',
   },
