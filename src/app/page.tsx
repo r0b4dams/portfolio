29c02418b0a55759c624b9e7c16ccaf7e66cb3cf
Robert Adams <adamsiii.robert@gmail.com>
2023-08-03 12:04:55 -0700

    use fn declaration & temp rm metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,7 @@
 import type { Metadata } from 'next';
 import { Greeting, IntroAnimation, Page } from '@/components';
 
-export const metadata: Metadata = {
-  title: 'Robert Adams | Portfolio',
-  description: 'hello',
-};
-
-const Intro: React.FC = () => {
+export default function Intro() {
   return (
     <Page>
       <main className='container w-screen h-screen flex flex-col-reverse justify-center items-center md:flex-row'>
@@ -17,6 +12,4 @@ const Intro: React.FC = () => {
       </main>
     </Page>
   );
-};
-
-export default Intro;
+}
