32b4d9c2f901dde0dfb5d6ccc4510c2e26dd531a
Robert Adams <adamsiii.robert@gmail.com>
2023-08-03 14:15:29 -0700

    resote single project page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,26 +1,55 @@
-"use client";
-
+import { Page } from '@/components/Page';
+import projects from '../projects.json';
+import { ProjectFeatures, ProjectGif, ProjectLinks, BadgeList as ProjectTech } from '@/components';
+import Link from 'next/link';
 interface Props {
   params: { id: string };
 }
 
-const Project: React.FC<Props> = ({ params }) => {
-  const { id } = params;
-
-  if (!id) {
-    return (
-      <div>
-        <p>404 | project not found</p>
-      </div>
-    );
+function getOneProject(id: string) {
+  const project = projects.find((project) => id === project.id);
+  if (project) {
+    return project as IProject;
   }
+  throw new Error('project not found');
+}
+
+export async function generateStaticParams() {
+  return projects.map((project) => ({ id: project.id }));
+}
+
+export default function Project({ params }: Props) {
+  const project = getOneProject(params.id);
 
   return (
-    <div>
-      <p>one project</p>
-      <p>{`project id: ${id}`}</p>
-    </div>
-  );
-};
+    <Page>
+      <Page.Heading>{project.name}</Page.Heading>
 
-export default Project;
+      <p className='font-thin text-xl'>{project.desc}</p>
+
+      <Link href='/projects' className='w-fit text-blue-400'>
+        ← back to projects
+      </Link>
+
+      <Page.Body>
+        <div className='flex flex-col lg:flex-wrap lg:flex-row-reverse md:w-full'>
+          <div className='self-center pt-5 lg:pt-0 lg:self-auto lg:w-1/2'>
+            <ProjectGif id={project.id} name={project.name} />
+          </div>
+
+          <div className='flex flex-col self-center md:w-[640px] lg:w-1/2 lg:self-auto'>
+            <ProjectLinks
+              projectName={project.name}
+              repoURL={project.repoURL}
+              appURL={project.appURL}
+            />
+            <ProjectFeatures features={project.features} />
+            <div className='mt-3'>
+              <ProjectTech title='Technologies' skills={project.tech} />
+            </div>
+          </div>
+        </div>
+      </Page.Body>
+    </Page>
+  );
+}
