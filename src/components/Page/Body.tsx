1f3f5f45d732cfb49d8dd343e4152d76a433fe47
Robert Adams <adamsiii.robert@gmail.com>
2023-08-02 18:16:42 -0700

    temp mv to legacy

--- src/components/Page/Body.tsx
+++
@@ -1,7 +0,0 @@
-interface IProps {
-  children: React.ReactNode;
-}
-
-export const Body: React.FC<IProps> = ({ children }) => {
-  return <div className="w-full md:flex grow">{children}</div>;
-};
--- src/middleware/logger.ts
+++
@@ -1,6 +0,0 @@
-import type { NextRequest } from "next/server";
-
-export const logger = (req: NextRequest) => {
-  const { method, nextUrl } = req;
-  console.log(method, nextUrl.pathname);
-};
