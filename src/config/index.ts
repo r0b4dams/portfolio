2a43eefe8fcb414c343a089702bb677ee698f9ca
Robert Adams <adamsiii.robert@gmail.com>
2023-08-03 12:46:31 -0700

    update vars with public prefix

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 export const CONFIG = {
-  EMAIL: process.env.EMAIL ?? '',
-  LINKEDIN_URL: process.env.LINKEDIN ?? '',
-  GITHUB_URL: process.env.GITHUB ?? '',
+  EMAIL: process.env.NEXT_PUBLIC_EMAIL ?? '',
+  LINKEDIN_URL: process.env.NEXT_PUBLIC_LINKEDIN_URL ?? '',
+  GITHUB_URL: process.env.NEXT_PUBLIC_GITHUB_URL ?? '',
+
   RESUME_URL: process.env.RESUME ?? '',
   PILLAR_URL: process.env.PILLAR ?? '',
+
   FEATURE: {
     ENABLE_DEPLOYED_LINK: process.env.ENABLE_DEPLOYED_LINK === 'true',
   },
