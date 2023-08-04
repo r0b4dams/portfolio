75941445c696c7f961f8a690feed52c87339b287
Robert Adams <adamsiii.robert@gmail.com>
2023-08-04 09:40:40 -0700

    temp rm color mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,19 +2,18 @@ import NextLink from 'next/link';
 
 import { Logo } from '../Logo';
 import { Nav } from '../Nav';
-import { ToggleDarkMode } from '../ToggleDarkMode';
+// import { ToggleDarkMode } from '../ToggleDarkMode';
 import { SpringBorder } from './SpringBorder';
 
 export const Header: React.FC = () => {
   return (
     <>
-      <header className='container flex items-center justify-between px-5'>
+      <header className='container flex items-center justify-between'>
         <NextLink href='/'>
           <Logo className='h-[10vh]' />
         </NextLink>
         <div className='flex items-center justify-center '>
           <Nav />
-          <ToggleDarkMode />
         </div>
       </header>
       <SpringBorder />
