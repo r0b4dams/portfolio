1fc44ccb676e0b542d5d95c7c6bd6b490b963a3f
Robert Adams <adamsiii.robert@gmail.com>
2023-08-12 23:23:24 -0700

    mv comment

--- src/animations/PageTransition.tsx
+++ src/animations/PageTransition.tsx
@@ -2,13 +2,14 @@
 
 import { animated, useTransition } from '@react-spring/web';
 
-// NODE: exit animation not functional currently
-// https://github.com/vercel/next.js/issues/49279#issuecomment-1541939624
 export function PageTransition({ children }: { children: React.ReactNode }) {
   const transition = useTransition(children, {
     delay: 500,
     from: { opacity: 0 },
     enter: { opacity: 1 },
+
+    // NOTE: exit animation not functional currently
+    // https://github.com/vercel/next.js/issues/49279#issuecomment-1541939624
     leave: { opacity: 1 },
     exitBeforeEnter: true,
   });
