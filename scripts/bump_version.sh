9e859b7121835597621e6fedcd3e09529a625606
Robert Adams <adamsiii.robert@gmail.com>
2023-08-13 23:10:47 -0700

    add scrips

---
+++ scripts/bump_version.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+shopt -s extglob
+
+config () {
+  yarn config set version-git-tag false
+  yarn config set version-tag-prefix ""
+}
+
+case $1 in
+  [Hh]otfix/?* | [Bb]ugfix/?* )
+    config
+    yarn version --patch
+    ;;
+  [Ff]eature/?* )
+    config
+    yarn version --minor
+    ;;
+  # Major incremented manually
+  !([Mm]ajor/?*) )
+    echo "error: invalid commit message pattern"
+    exit 1
+    ;;
+esac
---
+++ scripts/docker.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# docker login -u<username> -p<password>
+# docker build -t <username>/portfolio:<TAG> .
+# docker push <username>/portfolio:<TAG>
+
+# run an image locally
+# docker run -p 127.0.0.1:3000:3002 r0b4dams/portfolio:2.0.0
